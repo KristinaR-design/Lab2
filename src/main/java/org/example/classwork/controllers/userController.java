12f86694b2fd84ea98eedd022d71efea82b16830
Kristina <kristinarabova.36@gmail.com>
2024-09-16 14:16:35 +0500

    ghjkl;

--- src/main/java/org/example/classwork/controllers/userController.java
+++ src/main/java/org/example/classwork/controllers/userController.java
@@ -9,6 +9,6 @@ public class userController {
     @GetMapping("/{id}")
     public String getUserById(@PathVariable("id") String id) {
         // Return a message with the user ID
-        return "User with ID " + id + " found";
+        return "User with ID " + id + " founhd";
     }
 }
